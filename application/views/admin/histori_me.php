fe983b6dc7c2e72a2dc2ef6722293abe141ade43
Ryan Duta Pamungkas <arsuma24@gmail.com>
2019-12-18 09:25:51 +0700

    Histori View

---
+++ application/views/admin/histori_me.php
@@ -0,0 +1,16 @@
+
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0 text-dark">Histori Pengukuran</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="<?= site_url('admin') ?>">Home</a></li>
+                                <li class="breadcrumb-item active">Histori Pengukuran</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>

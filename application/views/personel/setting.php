0ce2f9f43cef4db2046b78069c185d17e9c31fe3
Rafly <mrafly75@gmail.com>
2019-12-19 11:48:09 +0700

    personel fix

--- application/views/personel/setting.php
+++ application/views/personel/setting.php
@@ -21,31 +21,31 @@
                         <div class="form-group">
                           <?= form_open('personel/setting') ?>
                             <label for="">NIP</label>
-                            <input type="text" class="form-control" disabled id="nip" value="<?= $data_personil->nip ?>">
+                            <input type="text" class="form-control" name="nip" disabled id="nip" value="<?= $data_personil->nip ?>">
                           </div>
                           <div class="form-group">
                             <label for="">ID Pegawai</label>
-                            <input type="text" class="form-control" id="pegawai" value="<?= $data_personil->id_pegawai ?>">
+                            <input type="text" class="form-control" name="id_pegawai" id="pegawai" value="<?= $data_personil->id_pegawai ?>">
                           </div>
                           <div class="form-group">
                             <label for="exampleInputEmail1">Nama</label>
-                            <input type="text" class="form-control" id="nama" value="<?= $data_personil->nama ?>">
+                            <input type="text" class="form-control" name="nama" id="nama" value="<?= $data_personil->nama ?>">
                           </div>
                           <div class="form-group">
                             <label for="exampleInputEmail1">Jabatan</label>
-                            <input type="text" class="form-control" id="jabatan" value="<?= $data_personil->jabatan ?>">
+                            <input type="text" class="form-control" name="jabatan" id="jabatan" value="<?= $data_personil->jabatan ?>">
                           </div>
                           <div class="form-group">
                             <label for="">Unit</label>
-                            <input type="text" class="form-control" id="unit" value="<?= $data_personil->unit ?>" >
+                            <input type="text" class="form-control" name="unit" id="unit" value="<?= $data_personil->unit ?>" >
                           </div>
                           <div class="form-group">
                             <label for="exampleInputPassword1">No.HP</label>
-                            <input type="text" class="form-control" id="no" value="<?= $data_personil->no ?>" >
+                            <input type="text" class="form-control" name="nama" id="no" value="<?= $data_personil->no ?>" >
                           </div>
                           <div class="form-group">
                             <label for="exampleInputPassword1">Email</label>
-                            <input type="text" class="form-control" id="email" value="<?= $data_personil->email ?>">
+                            <input type="text" class="form-control" name="email" id="email" value="<?= $data_personil->email ?>">
                           </div>
                     </div>
                           <div class="modal-footer justify-content-between">

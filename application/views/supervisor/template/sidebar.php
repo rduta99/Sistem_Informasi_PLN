e18719b96b950f5f7e39926ff9b65d793fed2169
Muhammad Imam Utama <m.imam.utama@gmail.com>
2020-01-03 10:35:52 +0700

    a

--- application/views/supervisor/template/sidebar.php
+++ application/views/supervisor/template/sidebar.php
@@ -1,10 +1,10 @@
 
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
-            <img src="<?= base_url('assets/') ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+            <a href="supervisor" class="brand-link">
+            <img src="<?= base_url('assets/') ?>dist/img/Logo_PLN.png" alt="PLN UIKSBS Logo" class="brand-image img-square elevation-2"
                 style="opacity: .8">
-            <span class="brand-text font-weight-light">AdminLTE 3</span>
+            <span class="brand-text font-weight-light">S I P</span>
             </a>
 
             <!-- Sidebar -->

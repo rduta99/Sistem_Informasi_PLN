3ba267457178e64e4026c1648ee19f73b1fa1950
Rafly <mrafly75@gmail.com>
2020-01-03 14:12:44 +0700

    zazazza

--- application/views/admin/template/title.php
+++ application/views/admin/template/title.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?= $title ?>Sistem Informasi Peralatan</title>
+    <title><?= $title ?>Sistem Informasi Peralatan Engineering PLN</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="<?= base_url('assets/') ?>plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/dist/img/bg') ?>">
     <!-- Tempusdominus Bbootstrap 4 -->
     <link rel="stylesheet" href="<?= base_url('assets/') ?>plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
     <!-- iCheck -->

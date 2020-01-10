05962060a4e377ce984a2e458f01625a995c114b
Muhammad Imam Utama <m.imam.utama@gmail.com>
2020-01-10 11:57:08 +0700

    posdiohgisidopajdfkbjhfddskfjnsfdbhgsjofpndgsdbhfsbfpdnsbjsvnkjhdsb

--- application/views/supervisor/tool.php
+++ application/views/supervisor/tool.php
@@ -170,10 +170,13 @@
                                             <?= $k->tgl_kalibrasi ?>
                                         </td>
                                         <td>
-                                            <button class="btn btn-sm btn-warning text-white" data-toggle="modal" data-target="#modal-<?= $k->id_tools ?>">
+                                            <a class="btn btn-sm btn-warning text-white" href="<?= site_url('supervisor/detail_tools/'.$k->id_tools) ?>">
                                                 <i class="fas fa-pen"></i>
-                                            </button>
-                                            <div class="modal fade" id="modal-<?= $k->id_tools ?>">
+                                            </a>
+                                            <!-- <button class="btn btn-sm btn-warning text-white" data-toggle="modal" data-target="#modal-<?= $k->id_tools ?>">
+                                                <i class="fas fa-pen"></i>
+                                            </button> -->
+                                            <!-- <div class="modal fade" id="modal-<?= $k->id_tools ?>">
                                                 <div class="modal-dialog">
 
                                                     <?= form_open("supervisor/tools_edit") ?>
@@ -224,7 +227,7 @@
                                                                     </span>
                                                                 </div>
                                                                 <input type="text" name="merk" class="form-control" value="<?= $k->merk ?>">
-                                                            </div>
+                                                            </div> -->
 
                                                             <!-- <div class="input-group mb-3">
                                                                 <div class="input-group-prepend">
@@ -240,7 +243,7 @@
                                                                 </select>
                                                             </div> -->
 
-                                                            <div class="input-group mb-3">
+                                                            <!-- <div class="input-group mb-3">
                                                                 <div class="input-group-prepend">
                                                                     <span class="input-group-text">
                                                                         <i class="fas fa-calendar-alt"></i>
@@ -260,7 +263,7 @@
                                                     <?= form_close() ?>
 
                                                 </div>
-                                            </div>
+                                            </div> -->
                                             <a href="<?= site_url('supervisor/delete_tools/'.$k->id_tools) ?>"class="btn btn-sm btn-danger text-white">
                                                 <i class="fas fa-trash"></i>
                                             </a>

c1ef0ad0838f1620c4f68f98d97c76ffc8bdc64a
rduta99 <arsuma24@gmail.com>
2020-01-16 14:46:21 +0700

    kipli

---
+++ application/views/supervisor/detail_ukur.php
@@ -0,0 +1,97 @@
+
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0 text-dark">List Analisis</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="<?= site_url('supervisor') ?>">Home</a></li>
+                                <li class="breadcrumb-item"><a href="<?= site_url('supervisor/his_pengukuran') ?>">Histori Pengukuran</a></li>
+                                <li class="breadcrumb-item active">Detail Pengukuran</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="timeline">
+                                <?php foreach ($pengukuran as $k) { ?>
+                                    <div class="time-label">
+                                        <span><?= date('d M Y', strtotime($k->waktu)) ?></span>
+                                    </div>
+                                    <div>
+                                        <i class="fas fa-stop-circle bg-blue"></i>
+                                        <div class="timeline-item">
+                                            <div class="card collapsed-card" style="height: inherit; width: inherit; transition: all 0.15s ease 0s;">
+                                                <div class="card-header">
+                                                    <h3 class="card-title"><?= $k->desk ?></h3>
+
+                                                    <div class="card-tools">
+                                                        <a href="<?= site_url('supervisor/detail_pengukuran/'.$k->id_pengukuran) ?>" class="btn btn-primary btn-sm">Detail</a>
+                                                        <button type="button" class="btn btn-tool" data-card-widget="maximize"><i class="fas fa-expand"></i></button>
+                                                    </div>
+                                                    
+                                                </div>
+                                                
+                                                <div class="card-body overflow-auto" style="display: none;">
+                                                    <div class="card">
+                                                        <div class="card-header border-0">
+                                                            <h3 class="card-title">General Drawing Peralatan & Titik Pengukuran</h3>
+                                                        </div>
+                                                        <div class="card-body">
+                                                            <?= $k->general_draw ?>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="card">
+                                                        <div class="card-header border-0">
+                                                            <h3 class="card-title">Finding</h3>
+                                                        </div>
+                                                        <div class="card-body">
+                                                            <?= $k->finding ?>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="card">
+                                                        <div class="card-header border-0">
+                                                            <h3 class="card-title">Diagnose</h3>
+                                                        </div>
+                                                        <div class="card-body">
+                                                            <?= $k->diagnose ?>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="card">
+                                                        <div class="card-header border-0">
+                                                            <h3 class="card-title">Analysis</h3>
+                                                        </div>
+                                                        <div class="card-body">
+                                                            <?= $k->analysis ?>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="card">
+                                                        <div class="card-header border-0">
+                                                            <h3 class="card-title">Recommendation</h3>
+                                                        </div>
+                                                        <div class="card-body">
+                                                            <?= $k->recommendation ?>
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
